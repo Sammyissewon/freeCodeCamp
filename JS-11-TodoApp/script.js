6541680c2cdfda41c078b523c2f71fec66c7f8f4
Sammyissewon <person1341@gmail.com>
2024-10-04 22:15:51 +0700

    Todo App 주석추가

--- JS-11-TodoApp/script.js
+++ JS-11-TodoApp/script.js
@@ -11,10 +11,14 @@ const dateInput = document.getElementById("date-input");
 const descriptionInput = document.getElementById("description-input");
 
 const taskData = JSON.parse(localStorage.getItem("data")) || [];
+
 let currentTask = {};
 
+// 할일 추가 및 업데이트 함수
 const addOrUpdateTask = () => {
   const dataArrIndex = taskData.findIndex((item) => item.id === currentTask.id);
+
+  // 할일 입력데이터 객체 저장
   const taskObj = {
     id: `${titleInput.value.toLowerCase().split(" ").join("-")}-${Date.now()}`,
     title: titleInput.value,
@@ -22,17 +26,21 @@ const addOrUpdateTask = () => {
     description: descriptionInput.value,
   };
 
+  // taskData에서 현재 작업중인 ID와 일치하는 게 없으면 -1을 반환 -> 즉, 새로운 할일을 추가한다는 것
   if (dataArrIndex === -1) {
     taskData.unshift(taskObj);
+    // 일치하는 ID가 있다 -> 기존 데이터를 업데이트 한다는 것
   } else {
     taskData[dataArrIndex] = taskObj;
   }
 
+  // localStorage에 데이터 저장
   localStorage.setItem("data", JSON.stringify(taskData));
   updateTaskContainer();
   reset();
 };
 
+// 할일 리스트 업데이트
 const updateTaskContainer = () => {
   tasksContainer.innerHTML = "";
 
@@ -49,6 +57,7 @@ const updateTaskContainer = () => {
   });
 };
 
+// 할일 삭제
 const deleteTask = (buttonEl) => {
   const dataArrIndex = taskData.findIndex(
     (item) => item.id === buttonEl.parentElement.id
@@ -59,6 +68,8 @@ const deleteTask = (buttonEl) => {
   localStorage.setItem("data", JSON.stringify(taskData));
 };
 
+// 할일 수정버튼 클릭시, 기존 데이터 불러와주는 함수
+// addOrUpdateTask 함수로 실제 데이터를 수정하기 전의 준비 단계
 const editTask = (buttonEl) => {
   const dataArrIndex = taskData.findIndex(
     (item) => item.id === buttonEl.parentElement.id
@@ -75,6 +86,7 @@ const editTask = (buttonEl) => {
   taskForm.classList.toggle("hidden");
 };
 
+// 리셋
 const reset = () => {
   addOrUpdateTaskBtn.innerText = "Add Task";
   titleInput.value = "";
@@ -88,18 +100,24 @@ if (taskData.length) {
   updateTaskContainer();
 }
 
+// 일정 작성창 열기 이벤트
 openTaskFormBtn.addEventListener("click", () =>
   taskForm.classList.toggle("hidden")
 );
 
+// 일정 작성창 닫기 이벤트
 closeTaskFormBtn.addEventListener("click", () => {
+  // 어떤 폼에라도 데이터가 있으면 true
   const formInputsContainValues =
     titleInput.value || dateInput.value || descriptionInput.value;
+
+  // 기존에 저장된 값과 현재 작성한 값이 다르면 true
   const formInputValuesUpdated =
     titleInput.value !== currentTask.title ||
     dateInput.value !== currentTask.date ||
     descriptionInput.value !== currentTask.description;
 
+  // 어떤 폼에라도 값이 있으면서, 기존 값과 다른 값이 있으면 모달을 띄움
   if (formInputsContainValues && formInputValuesUpdated) {
     confirmCloseDialog.showModal();
   } else {
